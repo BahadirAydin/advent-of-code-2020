f95610f27bccea42364e83f54d0ba9e56fbdfea8
BahadirAydin <bahadiraydin1923@gmail.com>
2022-10-20 23:42:30 +0300

    Added strategy and implemented some functions

--- day21/day21_A.go
+++ day21/day21_A.go
@@ -1,55 +1,104 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "regexp"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
 )
 
 type Item struct {
-  foods     []string
-  allergens []string
+	foods     []string
+	allergens []string
 }
 
 func ReadLines() []Item {
-  f, _ := os.Open("input.txt")
-  defer f.Close()
-  scanner := bufio.NewScanner(f)
-  arr := make([]Item, 0)
-  for scanner.Scan() {
-    arr = append(arr, Item{})
-    text := scanner.Text()
-    reg := regexp.MustCompile(`(?m)\bcontains\s+(.*)$`)
-    allergens := reg.FindString(text)
-    allergens = allergens[9:len(allergens)-1] + " "
-    str := ""
-    for i := 0; i < len(allergens); i++ {
-      if allergens[i] == ',' || allergens[i] == ' ' {
-        arr[len(arr)-1].allergens = append(arr[len(arr)-1].allergens, str)
-        str = ""
-        i++
-        continue
-      }
-      str += string(allergens[i])
-    }
-    str = ""
-    fmt.Println(allergens)
-    for _, v := range text {
-      if v == '(' {
-        break
-      }
-      if v == ' ' {
-        arr[len(arr)-1].foods = append(arr[len(arr)-1].foods, str)
-        str = ""
-        continue
-      }
-      str += string(v)
-    }
-  }
-  return arr
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	arr := make([]Item, 0)
+	for scanner.Scan() {
+		arr = append(arr, Item{})
+		text := scanner.Text()
+		reg := regexp.MustCompile(`(?m)\bcontains\s+(.*)$`)
+		allergens := reg.FindString(text)
+		allergens = allergens[9:len(allergens)-1] + " "
+		str := ""
+		for i := 0; i < len(allergens); i++ {
+			if allergens[i] == ',' || allergens[i] == ' ' {
+				arr[len(arr)-1].allergens = append(arr[len(arr)-1].allergens, str)
+				str = ""
+				i++
+				continue
+			}
+			str += string(allergens[i])
+		}
+		str = ""
+		fmt.Println(allergens)
+		for _, v := range text {
+			if v == '(' {
+				break
+			}
+			if v == ' ' {
+				arr[len(arr)-1].foods = append(arr[len(arr)-1].foods, str)
+				str = ""
+				continue
+			}
+			str += string(v)
+		}
+	}
+	return arr
 }
+
+/*
+Strategy:
+
+ 1. Compare two Item's :
+    1a. Get intersection of their "foods".
+    1b. Gen intersection of their "allergens".
+    1c. If both sets are non-empty, create a new item from those intersection of foods and allergens. Delete the intersection items from the original Items lists.
+ 2. Do Step 1 for all possible pairs of items until there is no more new Item emerging.
+ 3. Look at the Items with non-empty allergen list and the remove the elements of their foods list from other Items foods list with empty allergens list.
+ 4. Count the items with empty allergen-list.
+*/
+
+func intersection(list1 []string, list2 []string) (arr []string) {
+	for _, l1 := range list1 {
+		for _, l2 := range list2 {
+			if l1 == l2 {
+				arr = append(arr, l1)
+			}
+		}
+	}
+	return arr
+}
+func removeElems(orig []string, elems []string) (arr []string) {
+	flag := true
+	for _, v := range orig {
+		flag = true
+		for _, elem := range elems {
+			if v == elem {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			arr = append(arr, v)
+		}
+	}
+	return
+}
+func compare(i1 Item, i2 Item) (bool, Item) {
+	foodsIntersection := intersection(i1.foods, i2.foods)
+	allergensIntersection := intersection(i1.allergens, i2.allergens)
+	if len(foodsIntersection) == 0 || len(allergensIntersection) == 0 {
+		return false, Item{}
+	} else {
+		return true, Item{foods: foodsIntersection, allergens: allergensIntersection}
+	}
+}
+
 func main() {
-  data := ReadLines()
-  fmt.Println(data)
+	data := ReadLines()
+	fmt.Println(data)
 }
